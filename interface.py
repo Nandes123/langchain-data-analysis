b7a095ecfcd73abba3d112ebdc2699a6c9e2c913
Nandes123 <166856368+Nandes123@users.noreply.github.com>
2024-04-14 14:45:06 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -8,7 +8,7 @@ import streamlit as st
 import pandas as pd
 import json
 
-from agent import query_agent, create_agent
+from agents import query_agent, create_agent
 
 
 def decode_response(response: str) -> dict:
